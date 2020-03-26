50cb365abc9ccca704c63625274662274e9496f7
sushan <sushan.upadhyay@yahoo.com>
2020-03-27 01:16:51 +0530

    update

---
+++ src/ch03/automated_grading_system/grader.py
@@ -0,0 +1,22 @@
+class Assignment(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    
+    def lesson(self, student):
+        pass
+
+    @abc.abstactmethod
+    def check(self, code):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is Assignment:
+            attrs = set(dir(C))
+            if set(cls.__abstractmethods__) <= attrs:
+                return True
+
+        return NotImplemented
+
+
+
+
