3394d2d8a54f075b3f2e35bff988b5e8fc6cb44f
sushan <sushan.upadhyay@wns.com>
2020-03-26 18:43:55 +0530

    update

--- src/ch03/abstract_base_class_audio_14.py
+++ src/ch03/abstract_base_class_audio_14.py
@@ -1,5 +1,6 @@
 import abc
 
+
 class MediaLoader(metaclass=abc.ABCMeta):
     # @abc.abstractclassmethod # deprecated
     # source: https://docs.python.org/3/library/abc.html#abc.abstractstaticmethod
@@ -8,34 +9,36 @@ class MediaLoader(metaclass=abc.ABCMeta):
     def play(self):
         pass
 
-    # @abc.abstractproperty
+    # @abc.abstractproperty # deprecated
     @property
     @abc.abstractmethod
     def ext(self):
         pass
 
     @classmethod
-    def __subclasshook__(cls,C):
-        if cls is  MediaLoader:
-            attrs =  set(dir(C))
+    def __subclasshook__(cls, C):
+        if cls is MediaLoader:
+            attrs = set(dir(C))
             if set(cls.__abstractmethods__) <= attrs:
                 return True
 
         return NotImplemented
 
+
 class Wav(MediaLoader):
     pass
 
+
 class Ogg(MediaLoader):
     ext = ".ogg"
+
     def play(self):
+        print('This will play an ogg file')
         pass
 
 
 ## Since Wav class does not implement abstract attributes, its not possible to instantiate that class
-# o = Ogg()
-
-## Ogg provides both the attributes
 # w = Wav()
 
-    
+## Ogg provides both the attributes
+# o = Ogg()
---
+++ src/ch03/automated_grading_system/01.py
@@ -0,0 +1,9 @@
+class IntroPython:
+    def lesson(self):
+        return f"""
+            Hello {self.student}. Define two variables, an integer named a with value 1 and a  string named b with value 'hello'.
+        """
+
+    def check(self,code):
+        return code == "a = 1 \nb = 'hello'"
+
