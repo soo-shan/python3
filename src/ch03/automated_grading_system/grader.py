d6b49eeedae28551ec1a0f80cbaf704b97ecfd76
sushan <sushan.upadhyay@wns.com>
2020-03-27 20:29:39 +0530

    'update'

--- src/ch03/automated_grading_system/grader.py
+++ src/ch03/automated_grading_system/grader.py
@@ -1,10 +1,10 @@
+import abc
 class Assignment(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    
     def lesson(self, student):
         pass
 
-    @abc.abstactmethod
+    @abc.abstractmethod
     def check(self, code):
         pass
 
@@ -16,6 +16,31 @@ class Assignment(metaclass=abc.ABCMeta):
                 return True
 
         return NotImplemented
+
+
+class Statistics(Assignment):
+    """
+    Explicitly extend Assignment class with Statistics lesson
+    """
+    def lesson(self):
+        return(
+            "Good work so far,"
+            + self.student
+            + ". Now calculate the average of the numbers "
+            + " 1, 5, 18, -3 and assign to a variable named 'avg'"
+            )
+
+    def check(self,code):
+        import statistics
+
+        code = "import statistics\n" + code
+
+        local_vars = {}
+        global_vars = {}
+        exec(code, global_vars, local_vars)
+
+        return local_vars.get("avg") == statistics.mean([1, 5, 18, -3])
+
 
 
 
--- src/ch03/automated_grading_system/lessons.py
+++ src/ch03/automated_grading_system/lessons.py
@@ -1,3 +1,4 @@
+from grader import Assignment
 class IntroPython:
     def lesson(self):
         return f"""
@@ -6,4 +7,31 @@ class IntroPython:
 
     def check(self,code):
         return code == "a = 1 \nb = 'hello'"
+
+class Statistics(Assignment):
+    """
+    Explicitly extend Assignment class
+    """
+    def lesson(self):
+        return(
+            "Good work so far,"
+            + self.student
+            + ". Now calculate the average of the numbers "
+            + " 1, 5, 18, -3 and assign to a variable named 'avg'"
+            )
+
+    def check(self,code):
+        import statistics
+
+        code = "import statistics\n" + code
+
+        local_vars = {}
+        global_vars = {}
+        exec(code, global_vars, local_vars)
+
+        return local_vars.get("avg") == statistics.mean([1, 5, 18, -3])
+
+# Check IntroPython 
+print("issubclass(IntroPython,Assignment):",issubclass(IntroPython,Assignment))
+
 
